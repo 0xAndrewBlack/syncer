f49128e0ecb14df4427a568f5bce466b4ad8490c
0xAndrewBlack <lemon@zgen.hu>
2022-08-29 13:40:10 +0200

    ⚡ refactor: Dependabot cleaner.

--- src/discord/events/messageEvents.ts
+++ src/discord/events/messageEvents.ts
@@ -20,13 +20,14 @@ export class MessageHandler {
 			logger.debug(config.DB_CHANNEL);
 			logger.debug(message.channelId === config.DB_CHANNEL);
 			logger.debug(message.author?.id === config.DB_ID);
-			logger.debug(message.embeds[0].title === 'dependabot[bot]');
+			logger.debug(message.embeds[0].title == 'dependabot[bot]');
 			logger.debug(message.embeds[0].title);
 			logger.debug(message.embeds);
 			if (
 				message.embeds.length > 0 &&
 				message.author?.id == config.DB_ID &&
-				message.embeds[0].title == 'dependabot[bot]'
+				// @ts-ignore
+				message.embeds[0].title.contains('bot')
 			) {
 				message.delete();
 				logger.verbose('MESSAGE > Dependabot fuck off.');
