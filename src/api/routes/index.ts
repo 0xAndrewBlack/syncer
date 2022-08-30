33286d4dd4c234b280a9a1a7ee6752546e53d7c1
0xAndrewBlack <lemon@zgen.hu>
2022-08-30 15:23:45 +0200

    ⚡ refactor: Removed logging because we have morgan for it.

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -4,11 +4,9 @@ import { config } from '../../config.js';
 import type { RouterContext } from '@koa/router';
 import { Get, Middleware, Router } from '@discordx/koa';
 
-import { Log } from '../middlewares/Log.middleware.js';
 import { Authenticated } from '../middlewares/Auth.middleware.js';
 
 @Router()
-@Middleware(Log)
 export class Index {
 	@Get('/')
 	hello(ctx: RouterContext): void {
